42ff848dd5c80fa82a71e4aa241a6a31bf4b10e2
luis.goncalves <luis.goncalves@vivest.com.br>
2025-06-13 12:34:41 +0100

    Projeto executado localmente

--- ia_integration_aws/src/ia_integration_aws/crew.py
+++ ia_integration_aws/src/ia_integration_aws/crew.py
@@ -43,6 +43,5 @@ class IaIntegrationAws():
             agents=self.agents, # Automatically created by the @agent decorator
             tasks=self.tasks, # Automatically created by the @task decorator
             process=Process.sequential,
-            verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            verbose=True,            
         )
